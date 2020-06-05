354a6ccf3618478f138fbe2de8ed4f50b2f94fdc
luku <cuba3@163.com>
2020-06-05 12:26:08 +0800

    完善宜忌等第，调整诸事不宜

--- date2LunarData.py
+++ date2LunarData.py
@@ -1,7 +1,7 @@
 import datetime
 import lunar
 # 测试数据
-now=datetime.datetime(2020,6,8,12,30)
+now=datetime.datetime(2020,5,15,12,30)
 
 a=lunar.Lunar(now)
 dic={
@@ -43,15 +43,15 @@ dic={
     '忌':a.badThing,
     '时辰经络':a.meridians
 }
-i=1
-c=0
-now = datetime.datetime(2019, 1, 1, 23, 30)
-while i <= 367:
-    a = lunar.Lunar(now)
-    print(now)
-    print(a.angelDemon)
-    now += datetime.timedelta(days=1)
-    i += 1
-# for i in dic:
-#     midstr='\t'* (2- len(i) // 2)+':'+'\t'
-#     print(i,midstr,dic[i])
+# i=1
+# c=0
+# now = datetime.datetime(2019, 1, 1, 23, 30)
+# while i <= 367:
+#     a = lunar.Lunar(now)
+#     print(now)
+#     print(a.angelDemon)
+#     now += datetime.timedelta(days=1)
+#     i += 1
+for i in dic:
+    midstr='\t'* (2- len(i) // 2)+':'+'\t'
+    print(i,midstr,dic[i])
