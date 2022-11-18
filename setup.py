fa0188d4c3939cdc705b94645cd4101bab279fbd
luke <cuba3@163.com>
2022-11-18 11:47:12 +0800

    修改打包文件

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 setup(
     name='cnlunar',
-    version='0.0.9',
+    version='0.1.0',
     packages=['cnlunar'],
     url='https://github.com/OPN48/cnLunar',
     author='cuba3',
     author_email='cuba3@163.com',
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst', encoding='utf-8').read(),
 )
